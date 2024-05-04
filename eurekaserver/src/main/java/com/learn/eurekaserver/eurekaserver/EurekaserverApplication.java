2997be40887251cb8b339cf5600432694bc11535
MSreeN <khyathimnsree369@gmail.com>
2024-05-04 23:07:43 +0530

    Learnt how to register client with eureka server

--- eurekaserver/src/main/java/com/learn/eurekaserver/eurekaserver/EurekaserverApplication.java
+++ eurekaserver/src/main/java/com/learn/eurekaserver/eurekaserver/EurekaserverApplication.java
@@ -2,8 +2,11 @@ package com.learn.eurekaserver.eurekaserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+//To enable this app as eureka server-based service registry
 @SpringBootApplication
+@EnableEurekaServer
 public class EurekaserverApplication {
 
 	public static void main(String[] args) {
