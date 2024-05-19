6dc64bd2b74a5aa64f00ba0dc9bb143768837a60
MSreeN <khyathimnsree369@gmail.com>
2024-05-19 22:40:40 +0530

    changes and circuit breaker basic

--- employee-service/src/main/java/com/learn/employeeservice/service/impl/EmployeeServiceImpl.java
+++ employee-service/src/main/java/com/learn/employeeservice/service/impl/EmployeeServiceImpl.java
@@ -9,11 +9,13 @@ import com.learn.employeeservice.exceptions.ResourceNotFoundException;
 import com.learn.employeeservice.repository.EmployeeRepository;
 import com.learn.employeeservice.service.ApiClient;
 import com.learn.employeeservice.service.EmployeeService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import java.util.Optional;
 
@@ -31,6 +33,9 @@ public class EmployeeServiceImpl implements EmployeeService {
 
     @Autowired
     ApiClient apiClient;
+
+    @Autowired
+    WebClient webClient;
 
     @Override
     public EmployeeDto saveEmployee(EmployeeDto employeeDto) {
@@ -53,12 +58,18 @@ public class EmployeeServiceImpl implements EmployeeService {
         return employeeDto1;
     }
 
+    @CircuitBreaker(name = "${spring.application.name}", fallbackMethod = "defaultResponse")
     @Override
     public ApiResponseDto getEmployeeById(Long id) throws ResourceNotFoundException {
         Optional<Employee> employee = employeeRepository.findById(id);
         Employee employee1 = employee.orElseThrow(() -> new ResourceNotFoundException("No employee with" + "id "+ id));
-        DepartmentDto department =
-                apiClient.getByDepartmentCode(employee1.getDepartmentCode());
+//        DepartmentDto department =
+//                apiClient.getByDepartmentCode(employee1.getDepartmentCode());
+        DepartmentDto department = webClient.get()
+                .uri("http://localhost:8082/api/department/"+employee1.getDepartmentCode())
+                .retrieve()
+                .bodyToMono(DepartmentDto.class)
+                .block();
         EmployeeDto employeeDto = modelMapper.map(employee1, EmployeeDto.class);
         ApiResponseDto apiResponseDto = new ApiResponseDto();
         apiResponseDto.setEmployeeDto(employeeDto);
